f4322537e50d4096aade9262fa8bf89c3c54b35c
bergurth <bergur@hugarfostur.net>
2017-04-05 15:43:54 +0000

    code cleaned input script

---
+++ src/verd_andi/inputPERAPclean.py
@@ -0,0 +1,60 @@
+from xml.dom import minidom
+
+import sqlite3, csv
+from decimal import Decimal
+connection = sqlite3.connect("db.sqlite3")
+cursor =connection.cursor()
+
+
+xmldoc = minidom.parse('./E15-2_IS_Final_country_list/itemList.xml')
+itemlist = xmldoc.getElementsByTagName('item')
+
+for s in itemlist:
+	itemcols = [s.attributes['code'].value.encode('utf-8').decode('utf-8') , s.attributes['label'].value.encode('utf-8').decode('utf-8'), s.attributes['unit'].value.encode('utf-8').decode('utf-8')]
+	# input into db
+	cursor.execute("INSERT INTO perap_item (code, label, unit) VALUES (?,?,?);", itemcols)
+
+	chars = s.getElementsByTagName('characteristic')
+	for c in chars:
+		enName = c.attributes['enName'].value
+		name = c.attributes['name'].value
+		item = s.attributes['code'].value
+		lizt = [item, name, enName]
+		
+		isProp = False
+		specify = False
+
+		value = ""
+
+		try:
+			char_type = int(c.attributes['type'].value)
+			if (char_type):
+				lizt.append(char_type)
+		except:
+			pass
+
+		try:
+			isProp = c.attributes['isProperty'].value
+
+		except:
+			pass
+		try:
+			specify = c.attributes['specify'].value
+
+		except:
+			pass
+
+		try:
+			value = c.toxml().split(">")[1].split("<")[0].encode('utf-8').decode('utf-8')
+		except:
+			pass
+
+		lizt.append(True if isProp else False)
+		lizt.append(True if specify else False)
+		lizt.append(value if value else False)
+
+		cursor.execute("INSERT INTO perap_characteristic (item_id, name, enName, char_type, isProperty, specify, value) VALUES (?,?,?,?,?,?,?);", lizt) 
+
+
+connection.commit()
+connection.close()
