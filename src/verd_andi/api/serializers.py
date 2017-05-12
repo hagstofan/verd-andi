33179a79b1553c92f24f3d64b4c4e6b39757a904
bergurth <bergur@hugarfostur.net>
2017-05-12 13:02:24 +0000

    ItemObserver destroy API working

--- src/verd_andi/api/serializers.py
+++ src/verd_andi/api/serializers.py
@@ -1,8 +1,17 @@
 from rest_framework import serializers
-from survey.models import Item
+from survey.models import Item, ItemObserver
 
 class ItemSerializer(serializers.ModelSerializer):
 
 	class Meta:
 		model = Item
 		fields = '__all__'
+
+
+class ItemObserverSerializer(serializers.ModelSerializer):
+
+	class Meta:
+		model = ItemObserver
+		fields = '__all__'
+
+
--- src/verd_andi/api/urls.py
+++ src/verd_andi/api/urls.py
@@ -6,4 +6,6 @@ from rest_framework.urlpatterns import format_suffix_patterns
 
 urlpatterns = [
     url(r'^$', views.ItemList.as_view()),
+    url(r'^observer-items/(?P<pk>[0-9]+)/update$',views.ItemObserverUpdateAPIView.as_view()),
+    url(r'^observer-items/(?P<pk>[0-9]+)/destroy$',views.ItemObserverDestroyAPIView.as_view()),
 ]
