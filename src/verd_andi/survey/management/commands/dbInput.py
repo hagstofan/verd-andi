19b5ce32f78c8d3217f423c921b7beb3a9a27f59
Bergur <bergur.thorgeirsson@hagstofa.is>
2018-03-15 18:36:27 +0000

    deflake8ified urls and dbInput.py

--- src/verd_andi/survey/management/commands/dbInput.py
+++ src/verd_andi/survey/management/commands/dbInput.py
@@ -1,23 +1,17 @@
-import os, sys
-from django.core.management.base import BaseCommand, CommandError
-from survey.models import *
-from django.core.files import File
+import os
+from django.core.management.base import BaseCommand
 from django.conf import settings
-
 from xml.dom import minidom
-import csv
-from decimal import Decimal
-
-from survey.models import *
+from survey.models import Survey, Item, Characteristic
 
 """
-if(len(sys.argv) < 2):
-    print "usage: python manage.py input-pics "
-    sys.exit()
+    # if(len(sys.argv) < 2):
+    #     print "usage: python manage.py input-pics "
+    #     sys.exit()
 
 
-for filename in os.listdir("/"):
-    print(str(filename))
+    # for filename in os.listdir("/"):
+    #     print(str(filename))
 """
 
 
@@ -29,11 +23,9 @@ class Command(BaseCommand):
         parser.add_argument('file_path', nargs='+', type=str)
         parser.add_argument('survey_number', nargs='+', type=int)
 
-
     def handle(self, *args, **options):
-        #self.stdout.write("hey dudes" + " " + str(options['pic_directory'][0]))
         fpath = options['file_path'][0]
-        #self.stdout.write(os.path.isabs(fpath))
+        # self.stdout.write(os.path.isabs(fpath))
         survey = options['survey_number'][0]
         if(os.path.isabs(fpath)):
             # absolute path givern
@@ -45,18 +37,21 @@ class Command(BaseCommand):
                 print(filename)
                 self.parse_input(filename, survey)
 
-
     def parse_input(self, file, survey_id):
         xmldoc = minidom.parse(file)
         itemlist = xmldoc.getElementsByTagName('item')
         for s in itemlist:
-            print("adding item - " + str(s.attributes['code'].value.encode('utf-8').decode('utf-8')))
+            print("adding item - " + str(s.attributes['code']
+                  .value.encode('utf-8').decode('utf-8')))
             survey = Survey.objects.get(pk=survey_id)
             item = Item.objects.create(
-                code = s.attributes['code'].value.encode('utf-8').decode('utf-8'),
-                label = s.attributes['label'].value.encode('utf-8').decode('utf-8'),
-                unit = s.attributes['unit'].value.encode('utf-8').decode('utf-8'),
-                survey = survey,
+                code=s.attributes['code']
+                      .value.encode('utf-8').decode('utf-8'),
+                label=s.attributes['label']
+                       .value.encode('utf-8').decode('utf-8'),
+                unit=s.attributes['unit']
+                      .value.encode('utf-8').decode('utf-8'),
+                survey=survey,
                 )
             item.save()
 
@@ -65,55 +60,47 @@ class Command(BaseCommand):
             for c in chars:
                 enName = c.attributes['enName'].value
                 name = c.attributes['name'].value
-                item_code = s.attributes['code'].value
                 lizt = [item, name, enName]
-                
                 isProp = False
                 specify = False
-
                 value = ""
 
                 try:
                     char_type = int(c.attributes['type'].value)
                     if (char_type):
                         lizt.append(char_type)
-                except:
+                except KeyError:
                     pass
 
                 try:
                     isProp = c.attributes['isProperty'].value
 
-                except:
+                except KeyError:
                     pass
                 try:
                     specify = c.attributes['specify'].value
 
-                except:
+                except KeyError:
                     pass
 
                 try:
-                    value = c.toxml().split(">")[1].split("<")[0].encode('utf-8').decode('utf-8')
-                except:
+                    value = c.toxml().split(">")[1].split("<")[0]\
+                            .encode('utf-8').decode('utf-8')
+                except KeyError:
                     pass
 
                 lizt.append(True if isProp else False)
                 lizt.append(True if specify else False)
-                lizt.append(value if value else False)  
+                lizt.append(value if value else False)
 
                 # add the char.
                 characteristic = Characteristic.objects.create(
-                    item = item,
-                    name = name,
-                    enName = enName,
-                    char_type = char_type,
-                    isProperty = True if isProp else False,
-                    specify = True if specify else False,
-                    value = value,
+                    item=item,
+                    name=name,
+                    enName=enName,
+                    char_type=char_type,
+                    isProperty=True if isProp else False,
+                    specify=True if specify else False,
+                    value=value,
                 )
                 characteristic.save()
-
-
-
-
-
-
--- src/verd_andi/verd_andi/urls.py
+++ src/verd_andi/verd_andi/urls.py
@@ -1,17 +1,21 @@
-"""verd_andi URL Configuration
+"""
+    verd_andi URL Configuration
 
-The `urlpatterns` list routes URLs to views. For more information please see:
-    https://docs.djangoproject.com/en/1.10/topics/http/urls/
-Examples:
-Function views
-    1. Add an import:  from my_app import views
-    2. Add a URL to urlpatterns:  url(r'^$', views.home, name='home')
-Class-based views
-    1. Add an import:  from other_app.views import Home
-    2. Add a URL to urlpatterns:  url(r'^$', Home.as_view(), name='home')
-Including another URLconf
-    1. Import the include() function: from django.conf.urls import url, include
-    2. Add a URL to urlpatterns:  url(r'^blog/', include('blog.urls'))
+    # The `urlpatterns` list routes URLs to views.
+    # For more information please see:
+    #     https://docs.djangoproject.com/en/1.10/topics/http/urls/
+    # Examples:
+    # Function views
+    #     1. Add an import:  from my_app import views
+    #     2. Add a URL to urlpatterns:  url(r'^$', views.home, name='home')
+    # Class-based views
+    #     1. Add an import:  from other_app.views import Home
+    #     2. Add a URL to urlpatterns:  url(r'^$', Home.as_view(), name='home')
+    # Including another URLconf
+    #     1. Import the include() function:
+    #        from django.conf.urls import url, include
+    #     2. Add a URL to urlpatterns:  url(r'^blog/', include('blog.urls'))
+
 """
 from django.conf import settings
 from django.conf.urls.static import static
@@ -24,7 +28,7 @@ from django.views.generic import RedirectView
 
 urlpatterns = [
     url(r'^$', RedirectView.as_view(url='/survey/udash/'), name='index'),
-	#url(r'^$', home, name='index'),
+    # url(r'^$', home, name='index'),
     url(r'^admin/', admin.site.urls),
     url(r'^nested_admin/', include('nested_admin.urls')),
     url(r'^accounts/', include('registration.backends.default.urls')),
@@ -34,5 +38,9 @@ urlpatterns = [
 
 
 if settings.DEBUG:
-    urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
-    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+    urlpatterns += static(
+            settings.STATIC_URL,
+            document_root=settings.STATIC_ROOT)
+    urlpatterns += static(
+        settings.MEDIA_URL,
+        document_root=settings.MEDIA_ROOT)
