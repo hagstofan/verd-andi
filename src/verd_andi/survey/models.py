178c02e52f1c45cc07c3a5bb9b1f3dd926060002
Bergur <bergur.thorgeirsson@hagstofa.is>
2018-03-16 15:34:33 +0000

    models codestyle fixes

--- src/verd_andi/survey/models.py
+++ src/verd_andi/survey/models.py
@@ -5,18 +5,11 @@ from django.utils.encoding import python_2_unicode_compatible
 from django.contrib.auth.models import User
 
 import datetime
-from django.utils import timezone
 
-from django.db.models import Q
-from django import forms
 
-# Create your models here.
-import datetime
 YEAR_CHOICES = []
 for r in range(1980, (datetime.datetime.now().year+1)):
-    YEAR_CHOICES.append((r,r))
-
-
+    YEAR_CHOICES.append((r, r))
 
 
 def create_uuid():
@@ -26,7 +19,9 @@ def create_uuid():
 @python_2_unicode_compatible
 class Survey(models.Model):
     code = models.CharField(max_length=100)
-    year = models.IntegerField(max_length=4, choices=YEAR_CHOICES, default=datetime.datetime.now().year)
+    year = models.IntegerField(max_length=4,
+                               choices=YEAR_CHOICES,
+                               default=datetime.datetime.now().year)
     dataflowID = models.CharField(max_length=100)
     current = models.BooleanField()
 
@@ -37,25 +32,27 @@ class Survey(models.Model):
         return self.code+"-"+str(self.year)
 
 
-
 @python_2_unicode_compatible
 class Item(models.Model):
     code = models.CharField(max_length=20, primary_key=True)
     label = models.CharField(max_length=200)
     unit = models.CharField(max_length=100)
     survey = models.ForeignKey(Survey)
-    picture = models.ImageField(upload_to="item_pic_uploads"+"/"+str(create_uuid()), blank=True, null=True)
+    picture = models.ImageField(upload_to="item_pic_uploads" +
+                                "/"+str(create_uuid()),
+                                blank=True,
+                                null=True)
 
     class Meta:
         ordering = ['code']
 
-    
     def __str__(self):
         return self.label + "-" + self.code
 
+
 @python_2_unicode_compatible
 class ItemCommentary(models.Model):
-    item =    models.OneToOneField(
+    item = models.OneToOneField(
         Item,
         on_delete=models.CASCADE,
         primary_key=True,
@@ -73,12 +70,11 @@ class ItemCommentary(models.Model):
 class Characteristic(models.Model):
     name = models.CharField(max_length=200)
     enName = models.CharField(max_length=200)
-    char_type = models.IntegerField() # this field is called type in the xml
+    char_type = models.IntegerField()  # this field is called type in the xml
     isProperty = models.BooleanField(default=False)
     specify = models.BooleanField(default=False)
     item = models.ForeignKey(Item)
     value = models.CharField(max_length=500, blank=True)
-    
 
     def __str__(self):
         return self.name
@@ -90,12 +86,19 @@ class Observation(models.Model):
     obs_time = models.DateField()
     shop_type = models.IntegerField()
     shop_identifier = models.CharField(max_length=200)
-    flag = models.CharField(max_length=4 ,    choices=[('E','E'),('O','O')])
-    discount = models.CharField(max_length=4, choices=[('N','N'),('R','R'),('Q','Q'),('T','T')])
+    flag = models.CharField(max_length=4, choices=[('E', 'E'), ('O', 'O')])
+    discount = models.CharField(max_length=4,
+                                choices=[('N', 'N'),
+                                         ('R', 'R'),
+                                         ('Q', 'Q'),
+                                         ('T', 'T')])
     observed_price = models.DecimalField(decimal_places=1, max_digits=25)
     observed_quantity = models.DecimalField(decimal_places=1, max_digits=25)
     item = models.ForeignKey(Item)
-    observer = models.ForeignKey(User, blank=True, related_name='survey_observer', null=True)
+    observer = models.ForeignKey(User,
+                                 blank=True,
+                                 related_name='survey_observer',
+                                 null=True)
     obs_comment = models.CharField(max_length=300, blank=True)
     specified_characteristics = models.CharField(max_length=400, blank=True)
     survey = models.ForeignKey(Survey)
@@ -103,16 +106,19 @@ class Observation(models.Model):
     def __str__(self):
         return str(self.item) + " " + str(self.obs_time)
 
+
 @python_2_unicode_compatible
 class ObservedCharacteristic(models.Model):
-    observation = models.ForeignKey(Observation, on_delete = models.CASCADE)
+    observation = models.ForeignKey(Observation, on_delete=models.CASCADE)
     characteristic = models.ForeignKey(
         Characteristic,
-        on_delete = models.CASCADE,
-        #limit_choices_to = Characteristic.objects.filter(item = self.observation.item)
-        ) # characteristic here intended to be limited to  the characteristiics of the item that the observation refers to
+        on_delete=models.CASCADE,
+        # limit_choices_to =
+        # Characteristic.objects.filter(item = self.observation.item)
+        )  # characteristic here intended to be
+    # limited to  the characteristiics of
+    # the item that the observation refers to
     value = models.CharField(max_length=200, default="")
-
 
     def __str__(self):
         return str(self.characteristic)
@@ -129,11 +135,12 @@ class UserObservation(models.Model):
 
 @python_2_unicode_compatible
 class ItemObserver(models.Model):
-    user = models.ForeignKey(User, on_delete = models.CASCADE)
-    item = models.ForeignKey(Item, on_delete = models.CASCADE)
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    item = models.ForeignKey(Item, on_delete=models.CASCADE)
 
     def __str__(self):
         return str(self.item) + '--' + str(self.user)
+
 
 @python_2_unicode_compatible
 class Observer(User):
