1bdfe5dde2559a2c27c4251794074dcc82885561
Bergur <bergur.thorgeirsson@hagstofa.is>
2018-03-16 11:18:49 +0000

    style fixes to admin

--- src/verd_andi/survey/admin.py
+++ src/verd_andi/survey/admin.py
@@ -1,33 +1,41 @@
 from django.contrib import admin
 import nested_admin
-
-from django.contrib.auth.models import User
-# Register your models here.
-from .models import Observation, Item, UserObservation, Characteristic, Survey, ObservedCharacteristic, ItemObserver, Observer, ItemCommentary
+from .models import (
+        Observation,
+        Item,
+        Characteristic,
+        Survey,
+        ObservedCharacteristic,
+        ItemObserver,
+        Observer,
+        ItemCommentary
+    )
 
 
 class ObservedCharacteristicInline(nested_admin.NestedTabularInline):
     model = ObservedCharacteristic
-    #form = ObservedCharacteristicForm
+    # form = ObservedCharacteristicForm
 
 
 class ObservationInline(nested_admin.NestedTabularInline):
     model = Observation
     extra = 3
-    inlines = [ObservedCharacteristicInline,]
+    inlines = [ObservedCharacteristicInline, ]
+
 
 class CharacteristicInline(nested_admin.NestedTabularInline):
     model = Characteristic
     extra = 3
+
 
 class ItemCommentaryInline(nested_admin.NestedTabularInline):
     model = ItemCommentary
 
 
 class ItemAdmin(nested_admin.NestedModelAdmin):
-    inlines = [CharacteristicInline, ItemCommentaryInline, ObservationInline,]
-    exclude = ('characteristics',)
-    search_fields = ['code','label']    
+    inlines = [CharacteristicInline, ItemCommentaryInline, ObservationInline, ]
+    exclude = ('characteristics', )
+    search_fields = ['code', 'label']
     list_filter = (
         ('survey', admin.RelatedOnlyFieldListFilter),
     )
@@ -35,7 +43,7 @@ class ItemAdmin(nested_admin.NestedModelAdmin):
 
 class ObservationAdmin(nested_admin.NestedModelAdmin):
     model = Observation
-    inlines = [ObservedCharacteristicInline,]
+    inlines = [ObservedCharacteristicInline, ]
     list_filter = (
         ('observer', admin.RelatedOnlyFieldListFilter),
         ('survey', admin.RelatedOnlyFieldListFilter),
@@ -45,36 +53,42 @@ class ObservationAdmin(nested_admin.NestedModelAdmin):
 class SurveyAdmin(nested_admin.NestedModelAdmin):
     model = Survey
 
+
 class ObserverAdmin(nested_admin.NestedModelAdmin):
-    model = ItemObserver 
-    search_fields = ['item',]
-    raw_id_fields = ('item',)
+    model = ItemObserver
+    search_fields = ['item', ]
+    raw_id_fields = ('item', )
     list_filter = (
         ('user', admin.RelatedOnlyFieldListFilter),
         )
 
+
 class ItemObserverInline(nested_admin.NestedTabularInline):
     model = ItemObserver
-    raw_id_fields = ('item',)
+    raw_id_fields = ('item', )
     extra = 3
 
 
 class UserObserverAdmin(nested_admin.NestedModelAdmin):
     model = Observer
 
-    inlines = [ItemObserverInline,]
-    exclude = ('item',)
+    inlines = [ItemObserverInline, ]
+    exclude = ('item', )
+
 
 class CharacteristicAdmin(nested_admin.NestedModelAdmin):
     model = Characteristic
-    search_fields = ['id',]
-    inlines = [ObservedCharacteristicInline,]
+    search_fields = ['id', ]
+    inlines = [ObservedCharacteristicInline, ]
+
 
 class ObservedCharacteristicAdmin(nested_admin.NestedModelAdmin):
     model = ObservedCharacteristic
 
+
 class ItemCommentaryAdmin(nested_admin.NestedModelAdmin):
     model = ItemCommentary
+
 
 admin.site.register(Survey, SurveyAdmin)
 admin.site.register(Item, ItemAdmin)
