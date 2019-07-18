18d5b7ff4f4d58f876a5f2dd9973e8a143a60289
Tryggvi Björgvinsson <tryggvi.bjorgvinsson@hagstofa.is>
2019-07-18 14:25:46 +0000

    Create superuser in docker development environment
    Create an admin user with an admin password for development via
    Docker.

--- dev-entrypoint.sh
+++ dev-entrypoint.sh
@@ -6,5 +6,6 @@ python manage.py makemigrations
 
 python manage.py migrate
 
+python manage.py shell -c "from django.contrib.auth.models import User; print('admin user exists') if User.objects.filter(username='admin').count() else User.objects.create_superuser('admin', 'verdandi@hagstofa.is', 'admin')"
+
 python manage.py runserver 0.0.0.0:8000 
-#./manage.py shell -c "from django.contrib.auth.models import User; User.objects.create_superuser('rabbit', 'rabbit@hagstofa.is', 'rabbit')"
