ae375815f3f9898e53f71aec3d0159f87f2e7744
Hermann Björgvin <hermann.haraldsson@hagstofa.is>
2019-07-18 11:43:20 +0000

    Added docker compose file

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 
+python manage.py makemigrations
+
 python manage.py migrate
 
-./manage.py shell -c "from django.contrib.auth.models import User; User.objects.create_superuser('rabbit', 'rabbit@hagstofa.is', 'rabbit')"
+#./manage.py shell -c "from django.contrib.auth.models import User; User.objects.create_superuser('rabbit', 'rabbit@hagstofa.is', 'rabbit')"
